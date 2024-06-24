a393f2c2ffdf4a174db8ae6d632e2763a440821e
stormyark <stormyark@gmail.com>
2024-06-24 17:03:02 +0200

    Quartz sync: Jun 24, 2024, 5:03 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,8 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          Created by <a href="https://stormyark.de">stormyark</a>, © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
