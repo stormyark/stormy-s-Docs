00252b9716de69c0d706769cf11532818537ef72
stormyark <stormyark@gmail.com>
2024-06-28 08:37:55 +0200

    Quartz sync: Jun 28, 2024, 8:37 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       Discord: "http://discordapp.com/users/755535092910129172",
-      "Raise and Issue on Github": "https://github.com/stormyark/stormy-s-Docs/issues/new",
+      "Raise an Issue on Github": "https://github.com/stormyark/stormy-s-Docs/issues/new",
     },
   }),
 }
